8505a468a23766cf1db18b0aa0b9c82aae669f18
NMShihab <nmshihabislam@gmail.com>
2024-10-29 23:20:21 +0600

    Add buttons

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import CartaddIcon from "./icons/CartaddIcon";
 import EyeIcon from "./icons/EyeIcon";
+import OfferIcon from "./icons/OfferIcon";
 
 interface ProductCardProps {
   title: string;
@@ -24,9 +25,18 @@ const ProductCard: React.FC<ProductCardProps> = ({
     <div className=" rounded-lg p-4 text-black relative group">
       <div className="relative">
         {discount && (
-          <span className="absolute top-2 left-2 bg-orange-500 text-white px-2 py-1 text-sm rounded">
-            ৳ {discount}
-          </span>
+          <div className="absolute top-2  z-20 text-white  text-sm rounded">
+            <div className="relative">
+              <OfferIcon />
+              <span className="absolute text-[10px] top-1 left-2">
+                - ৳ {discount}
+              </span>
+            </div>
+          </div>
+
+          // <span className="absolute top-2 left-2 bg-orange-500 text-white px-2 py-1 text-sm rounded">
+          //   ৳ {discount}
+          // </span>
         )}
 
         {/* Image container with hover effects */}
@@ -41,7 +51,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
           <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
 
           {/* Glass effect buttons on hover */}
-          <div className="absolute inset-0 flex flex-col gap-2 items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <div className="absolute inset-0 flex flex-col gap-2 items-center justify-end pb-4  opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             <button className="bg-white flex flex-row items-center justify-center gap-2 w-[80%] bg-opacity-20 backdrop-blur-sm text-white py-2 px-4 rounded-lg hover:bg-opacity-30 transition-colors">
               <CartaddIcon /> Add to Cart
             </button>
